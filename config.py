b5f610267d8627bb25abfe6a14b8a7c453ef96e9
Nikhil Buduma <nkbuduma@gmail.com>
2017-06-24 00:06:36 -0700

    first run at flask app

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'postgres://nikhil:ihtfp@localhost/avabackend?sslmode=disable'
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
